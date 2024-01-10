08813f0368f625ce70127988e2add62839748bee
elv-zenia <zenia.villa@eluv.io>
2024-01-10 14:36:35 -0800

    Add try/catch

--- src/stores/EditStore.js
+++ src/stores/EditStore.js
@@ -264,37 +264,41 @@ class EditStore {
   });
 
   UpdateStreamLink = flow(function * ({objectId, slug}) {
-    const originalLink = yield this.client.ContentObjectMetadata({
-      libraryId: dataStore.siteLibraryId,
-      objectId: dataStore.siteId,
-      metadataSubtree: `public/asset_metadata/live_streams/${slug}`,
-    });
+    try {
+      const originalLink = yield this.client.ContentObjectMetadata({
+        libraryId: dataStore.siteLibraryId,
+        objectId: dataStore.siteId,
+        metadataSubtree: `public/asset_metadata/live_streams/${slug}`,
+      });
 
-    const link = this.CreateLink({
-      targetHash: yield this.client.LatestVersionHash({objectId}),
-      options: originalLink
-    });
+      const link = this.CreateLink({
+        targetHash: yield this.client.LatestVersionHash({objectId}),
+        options: originalLink
+      });
 
-    const {writeToken} = yield this.client.EditContentObject({
-      libraryId: dataStore.siteLibraryId,
-      objectId: dataStore.siteId
-    });
+      const {writeToken} = yield this.client.EditContentObject({
+        libraryId: dataStore.siteLibraryId,
+        objectId: dataStore.siteId
+      });
 
-    yield this.client.ReplaceMetadata({
-      libraryId: dataStore.siteLibraryId,
-      objectId: dataStore.siteId,
-      writeToken,
-      metadataSubtree: `public/asset_metadata/live_streams/${slug}`,
-      metadata: link
-    });
+      yield this.client.ReplaceMetadata({
+        libraryId: dataStore.siteLibraryId,
+        objectId: dataStore.siteId,
+        writeToken,
+        metadataSubtree: `public/asset_metadata/live_streams/${slug}`,
+        metadata: link
+      });
 
-    yield this.client.FinalizeContentObject({
-      libraryId: dataStore.siteLibraryId,
-      objectId: dataStore.siteId,
-      writeToken,
-      commitMessage: "Update stream link",
-      awaitCommitConfirmation: true
-    });
+      yield this.client.FinalizeContentObject({
+        libraryId: dataStore.siteLibraryId,
+        objectId: dataStore.siteId,
+        writeToken,
+        commitMessage: "Update stream link",
+        awaitCommitConfirmation: true
+      });
+    } catch(error) {
+      console.error("Unable to update stream link", error);
+    }
   });
 
   DeleteStream = flow(function * ({objectId}) {
