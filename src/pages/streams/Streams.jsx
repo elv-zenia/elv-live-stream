b58b2c6b681470ff999bff3a4cf6a5dc227c956e
Zenia Villa <91760982+elv-zenia@users.noreply.github.com>
2024-09-20 09:07:19 -0700

    Display slug if no object id or title (#56)

--- src/pages/streams/Streams.jsx
+++ src/pages/streams/Streams.jsx
@@ -125,8 +125,8 @@ const Streams = observer(() => {
           columns={[
             { accessor: "title", title: "Name", sortable: true, render: record => (
               <div className="table__multi-line">
-                <Link to={`/streams/${record.objectId}`}>
-                  <Text fw={600}>{record.display_title}</Text>
+                <Link to={`/streams/${record.objectId || record.slug}`}>
+                  <Text fw={600}>{record.display_title || record.slug}</Text>
                 </Link>
                 <Text c="dimmed" fz="xs">{record.objectId}</Text>
               </div>
